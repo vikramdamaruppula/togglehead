f7b1f470cc853dae59c7c1f0fa3cb979850ab844
unknown <vikramd0652@gmail.com>
2024-06-30 14:31:56 +0530

    responsiveness for FAQs section for toggleg head assignment

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Faq = () => {
   return (
     <div className='faq-main'>
-        <h2 style={{textAlign:'center'}}> FAQ  </h2> 
+        <h2 className='faq-text'> FAQ  </h2> 
         <div className='faq-container'>
           <p>How does an investor gain access to MF Utility</p>
           <i class="fa-solid fa-circle-plus fa-2xl"></i>
